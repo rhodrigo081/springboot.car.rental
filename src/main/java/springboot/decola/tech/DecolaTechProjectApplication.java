b43c62eaec875c6b7adcca08c342978f82adb764
rhodrigo081 <rhodrigo.leandro@gmail.com>
2025-03-17 18:28:16 -0300

    First uploading

---
+++ src/main/java/springboot/decola/tech/DecolaTechProjectApplication.java
@@ -0,0 +1,13 @@
+package springboot.decola.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTechProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboot/decola/tech/DecolaTechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.decola.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
